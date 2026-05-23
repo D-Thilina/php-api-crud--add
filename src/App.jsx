0ce9e1361a647d5a360353060a164d4204fd1f95
thilina <thilinaudayanga1010@gmail.com>
2026-05-23 16:38:57 +0530

    Add some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-
+import {BrowserRouter , Routes , Route , Link} from 'react-router';
 
 function App() {
 
   return (
     <>
-      <h1>Hello react new one</h1>
+      <div
+        className={
+          " h-screen text-black bg-linear-to-r from-pink-400 via-blue-200 to-red-300"
+        }
+      >
+        <h5 className={"text-xl font-semibold text-center"}>
+          React CRUD Operations using PHP api and MySQL
+        </h5>
+
+        <BrowserRouter>
+          <nav className={"flex justify-center mt-5"}>
+            <ul className={"flex justify-around w-96"}>
+              <li className={"p-2 rounded-3xl bg-blue-400  w-24.25 text-center"}>
+                <Link to={"/"}>List User</Link>
+              </li>
+
+              <li className={"p-2 rounded-3xl bg-blue-400"}>
+                <Link to={"user/create"}>Create User</Link>
+              </li>
+            </ul>
+          </nav>
+        </BrowserRouter>
+      </div>
     </>
-  )
+  );
 }
 
 export default App
---
+++ src/components/ListUser.jsx
@@ -0,0 +1,10 @@
+
+const ListUser = () => {
+  return (
+    <div>
+        <h1>List User</h1>
+    </div>
+  );
+};
+
+export default ListUser;
